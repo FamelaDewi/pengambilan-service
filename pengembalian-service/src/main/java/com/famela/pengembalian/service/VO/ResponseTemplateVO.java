55af06d4ed5683538f9e415437ec6bcf111dd240
FamelaDewi <113493540+FamelaDewi@users.noreply.github.com>
2023-01-04 18:33:04 +0700

    Add files via upload

---
+++ pengembalian-service/src/main/java/com/famela/pengembalian/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.famela.pengembalian.service.VO;
+
+import com.famela.pengembalian.service.entity.Pengembalian;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author HP
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private Pengembalian pengembalian;
+    private Peminjaman peminjaman;
+}
